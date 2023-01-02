e01df57220282963f9edf1173cd64197905485e1
EliorCohenApps <elior@glueloyalty.com>
2023-01-02 21:44:04 +0200

    Update.

--- lib/presentation/pages/page_third.dart
+++ lib/presentation/pages/page_third.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../main.dart';
+import '../state_management/riverpod/provider_third.dart';
 
 class PageThird extends HookConsumerWidget {
   const PageThird({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ProviderThird();
+
     return Column(
       children: [
         Text(ref.watch(showAppBar) ? 'Show' : 'Not Show'),
+        ElevatedButton(
+          onPressed: () {
+            provider.counter.value++;
+          },
+          child: Text('${provider.counter.value}'),
+        ),
       ],
     );
   }
---
+++ lib/presentation/state_management/riverpod/provider_third.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class ProviderThird {
+  final counter = useState(0);
+}
